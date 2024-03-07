68231c95bb4dc4e2b5d6b8819160906818631f33
Jair <carvalhojair3000@gmail.com>
2024-03-07 20:27:55 -0300

    style: :art: alteração ícone botão
    mudança do botão para um estilo arredondado e preenchido

--- lib/main.dart
+++ lib/main.dart
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_circle_sharp),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
