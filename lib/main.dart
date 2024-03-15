dea76ff1778851cf67eddd56910f3835800f328a
Wilmar Lima <elasawilmar@gmail.com>
2024-03-15 15:59:23 -0300

    feat: os desafios 10, 12 e extra

--- lib/main.dart
+++ lib/main.dart
@@ -74,16 +74,27 @@ class _MyHomePageState extends State<MyHomePage> {
     return '';
   }
 
-  String desafio10() {
-    return '';
+  void _desafio10(String nome, int idade) {
+    setState(() {
+      if (idade >= 18) {
+        resultado = '$nome é maior de idade';
+      } else {
+        resultado = '$nome é menor de idade';
+      }
+    });
   }
 
   String desafio11() {
     return '';
   }
 
-  String desafio12() {
-    return '';
+  void _desafio12(List<int> numeros) {
+    setState(() {
+      for (int i = 0; i < numeros.length; i++) {
+        numeros[i] = numeros[i] * numeros[i];
+      }
+      resultado = numeros.join(',');
+    });
   }
 
   String desafio13() {
@@ -114,6 +125,55 @@ class _MyHomePageState extends State<MyHomePage> {
     return '';
   }
 
+  bool _d19comparaMesmasLetras(String principal, String secundaria) {
+    String letra = 'x';
+    bool contem = true;
+    if (principal.length != secundaria.length) {
+      return false;
+    }
+    for (int i = 0; i < principal.length; i++) {
+      letra = principal[i].toLowerCase();
+      if (secundaria.toLowerCase().contains(letra)) {
+        contem = true;
+      } else {
+        contem = false;
+        break;
+      }
+    }
+    return contem;
+  }
+
+  void _d19listaStrings(List<String> lista) {
+    setState(() {
+      List<List<String>> novaLista = [];
+      List<String> copiaLista = [];
+      List<bool> stringUsada = List.filled(lista.length, true);
+      bool comparou = true;
+
+      copiaLista = List.from(lista);
+      for (int i = 0; i < lista.length; i++) {
+        //
+        if (stringUsada[i] == true) {
+          novaLista.add([]);
+          List<String> novaSubLista = novaLista.last;
+          novaSubLista.add(lista[i]);
+          stringUsada[i] = false;
+
+          for (int j = 0; j < copiaLista.length; j++) {
+            if (stringUsada[j] == true) {
+              comparou = _d19comparaMesmasLetras(lista[i], copiaLista[j]);
+              if (comparou) {
+                novaSubLista.add(copiaLista[j]);
+                stringUsada[j] = false;
+              }
+            }
+          }
+        }
+      }
+      resultado = novaLista.map((sublist) => sublist.join('.')).join(', ');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -140,6 +200,18 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //Aqui vai chamar função
+          //_desafio10('Wilmar', 50);
+          //_desafio12([1, 2, 3]);
+          /* _d19listaStrings([
+            'foR',
+            'scream',
+            'CaRs',
+            'poTatos',
+            'racs',
+            'creams',
+            'scar',
+            'four'
+          ]); */
         },
         tooltip: 'Incrementar',
         child: const Icon(Icons.add_circle_sharp),
